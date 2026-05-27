95340f34532fc301a98dbcdc69a1595a162ed029
AceGenera1 <krisc755@gmail.com>
2026-05-27 14:10:06 -0400

    Update [id].js

--- api/api/devotional-share/[id].js
+++ api/api/devotional-share/[id].js
@@ -1,6 +1,6 @@
 const SITE_URL = "https://restoreddailyministries.org";
 const API_URL = "https://script.google.com/macros/s/AKfycbyMpMm_C9r3yF16VVKRmxNQmCmQaiLEDkqezOTSwpSjSsg6CGCuTx1Iw0JfMA5GmnKF/exec?type=devotionals";
-
+const ogImage = `${SITE_URL}/social-preview-devotional.png?v=100`;
 function esc(value = "") {
   return String(value)
     .replace(/&/g, "&amp;")
