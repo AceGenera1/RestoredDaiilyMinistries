dbbe52941d522e39f957d41ed9aa4457de7ad4bb
AceGenera1 <krisc755@gmail.com>
2026-05-27 13:24:49 -0400

    Update share-devotional.js

--- api/share-devotional.js
+++ api/share-devotional.js
@@ -1,6 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyMpMm_C9r3yF16VVKRmxNQmCmQaiLEDkqezOTSwpSjSsg6CGCuTx1Iw0JfMA5GmnKF/exec";
 const SITE_URL = "https://restoreddailyministries.org";
-
+const ogImage = `${SITE_URL}/social-preview-devotional.PNG?v=8`;
 function esc(value = "") {
   return String(value)
     .replace(/&/g, "&amp;")
